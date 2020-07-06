2508ddaeb21f73c281b114d9c4784d529bf0dc10
Muhammad Sufyan <51535061+SamiSahai@users.noreply.github.com>
2020-07-05 16:27:43 -0800

    created covid tracker app
    created covid tracker app with importing axios to fetch data, @material UI for styling card, chart and native selection bar for country picker etc. Moreover use effect funtion, use async await function to fetch and load data.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React from "react";
 
-import "./App.css";
+import { Cards, CountryPicker, Chart } from "./components";
+import { fetchData } from "./api/";
+import styles from "./App.module.css";
 
-function App() {
-  return <div className="App">COVID 19 App By Sufyan</div>;
+class App extends React.Component {
+  state = {
+    data: {},
+    country: "",
+  };
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  };
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/AppBar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import styles from "./AppBar.module.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    justifyContent: "center",
+  },
+}));
+
+export default function DenseAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit" text-align="center">
+            COVID Tracker By Sufyan
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <styles />
+    </div>
+  );
+}
